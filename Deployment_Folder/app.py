439b9f790fdee20c463e0a38fcb5e51694af8c30
Tashanam Shahbaz <64039299+Tashanam-Shahbaz@users.noreply.github.com>
2022-10-25 18:17:43 +0500

    Update app.py

--- Deployment_Folder/app.py
+++ Deployment_Folder/app.py
@@ -137,4 +137,4 @@ def model_3():
 
 if __name__=="__main__":
     port=int(os.environ.get('PORT',5000))
-    app.run(host="0.0.0.0",port=port,debug=True)
+    app.run(host="0.0.0.0",port=port,debug=False)
